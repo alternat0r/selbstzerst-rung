9166c1fea3caa255d367f9ac54ae3f35b6000f15
alternat0r <alternator99@gmail.com>
2014-04-09 13:19:14 +0800

    Working with designing first.

---
+++ Selbstzerstörung/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class frmMain
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' ||Requirement||
+        ' must be running with administrator priv.
+
+        ' ||THINGS TO DESTROY||
+        ' terminate gdrive process
+        ' delete mydocument
+        ' delete gdrive folder
+        ' delete other than C:\ drive
+        ' clear history, cache, temp, download folder
+        ' delete important software
+        ' --truecrypt
+        ' --visual studio
+        ' --vmware / virtualbox
+        ' --quassel
+        ' --vpn software
+        ' --web server
+        ' --torrent
+        ' all deleting process must be wipe with 0 byte
+        ' remove possible pirate software
+        ' self-destroy appsz
+
+    End Sub
+End Class
